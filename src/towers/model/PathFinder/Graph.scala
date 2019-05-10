fd843253c0ef9110682d9756b6931c72e4628add
xuanlang1216 <xuanlang1216@gmail.com>
2019-05-09 23:32:43 -0400

    x

---
+++ src/towers/model/PathFinder/Graph.scala
@@ -0,0 +1,62 @@
+package towers.model.PathFinder
+
+class Graph[A] {
+
+  var nodes: Map[Int, A] = Map()
+  var adjacencyList: Map[Int, List[Int]] = Map()
+
+
+  def addNode(index: Int, a: A): Unit = {
+    nodes += index -> a
+    adjacencyList += index -> List()
+  }
+
+  def addEdge(index1: Int, index2: Int): Unit = {
+    adjacencyList += index1 -> (index2 :: adjacencyList(index1))
+    adjacencyList += index2 -> (index1 :: adjacencyList(index2))
+  }
+
+  def areConnected(index1: Int, index2: Int): Boolean = {
+    var explored: Set[Int] = Set(index1)
+
+    val toExplore: Queue[Int] = new Queue()
+    toExplore.enqueue(index1)
+
+    while (!toExplore.empty()) {
+      val nodeToExplore = toExplore.dequeue()
+      for (node <- this.adjacencyList(nodeToExplore)) {
+        if(!explored.contains(node)){
+          //println("exploring: " + this.nodes(node))
+          toExplore.enqueue(node)
+          explored = explored + node
+        }
+      }
+    }
+    explored.contains(index2)
+  }
+
+  def distance(index1: Int, index2: Int): Int = {
+    var explored: Set[Int] = Set(index1)
+    var Distance:Map[Int,Int]=Map()
+    Distance=Distance+(index1->10000000)
+    val toExplore: Queue[Int] = new Queue()
+    toExplore.enqueue(index1)
+    while (!toExplore.empty()) {
+      val nodeToExplore = toExplore.dequeue()
+      for (node <- this.adjacencyList(nodeToExplore)) {
+        if(!explored.contains(node)){
+          toExplore.enqueue(node)
+          explored = explored + node
+          Distance = Distance + (node->nodeToExplore)
+        }
+      }
+    }
+    var shortestPath:List[Int]=List()
+    var current=index2
+    while(Distance(current)!=10000000){
+      shortestPath=Distance(current)::shortestPath
+      current=Distance(current)
+    }
+    shortestPath.size
+  }
+}
--- src/towers/model/ai/AIPlayer.scala
+++ src/towers/model/ai/AIPlayer.scala
@@ -3,6 +3,7 @@ package towers.model.ai
 import play.api.libs.json.{JsValue,Json}
 import towers.model.GridLocation
 import towers.model.physics.PhysicsVector
+import towers.model.PathFinder.Graph
 
 class AIPlayer(id: String) {
 
@@ -15,13 +16,57 @@ class AIPlayer(id: String) {
 
   def getPath(jsonGameState: String): List[GridLocation] = {
     //TODO
+    val pathgraph:Graph[GridLocation]=new Graph()
     val parsed:JsValue=Json.parse(jsonGameState)
     val players=(parsed\"players").as[List[JsValue]]
-    val walls=(parsed\"wall").as[List[JsValue]]
-    val base=(parsed\"base").as[List[JsValue]]
+    var AIx:Int=0
+    var AIy:Int=0
+    for(p<-players){
+      if((p\"id").as[String]==id){
+        AIx=(p\"x").as[Double].toInt
+        AIy=(p\"y").as[Double].toInt
+      }
+    }
+    val walls=(parsed\"walls").as[List[JsValue]]
+    var the_wall:List[GridLocation]=List()
+    var nodeindex=0
+    for(w<-walls){
+      the_wall=new GridLocation((w\"x").as[Int],(w\"y").as[Int])::the_wall
+    }
+    val base=(parsed\"base").as[Map[String,Int]]
+    val startloc=(parsed\"start").as[Map[String,Int]]
+    val gridsize=(parsed\"gridSize").as[Map[String,Int]]
+    val gridHeight=gridsize("y")
+    val gridWidth=gridsize("x")
+
+    for(y<- 0 until gridHeight){
+      for(x<-0 until gridWidth){
+        pathgraph.addNode(nodeindex,new GridLocation(x,y))
+        nodeindex+=1
+      }
+    }
+    for((node,grid)<-pathgraph.nodes){
+      pathgraph.addEdge(node,node+1)
+      pathgraph.addEdge(node,node-1)
+      pathgraph.addEdge(node,node-gridWidth)
+      pathgraph.addEdge(node,node+gridWidth)
+    }
+
+
+
+
 
 
     List()
+  }
+
+  def NotAWall(x:Int,y:Int,wall:List[GridLocation]):Boolean={
+    for(w<-wall){
+      if(x==w.x && y==w.y){
+        false
+      }
+    }
+    true
   }
 
   def pathToDirection(jsonGameState: String, path: List[GridLocation]): PhysicsVector = {
